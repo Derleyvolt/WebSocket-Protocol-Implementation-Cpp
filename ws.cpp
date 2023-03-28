56c6f7b97af1589a3c97ecb14a81f883c1a25af6
Wanderley <100Palavr@gmail.com>
2023-03-28 16:02:32 -0300

    Create ws.cpp

---
+++ ws.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+class ws {
+public:
+    ws() {
+
+    }
+
+
+    onConnection() {
+
+    }
+
+    onMessage() {
+
+    }
+
+    onClose() {
+        
+    }
+private:
+    int32_t fd;
+};
