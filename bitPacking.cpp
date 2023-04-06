5453d62bbf2dc1045b0d6e3515dce5b65117d4f7
Wanderley <100Palavr@gmail.com>
2023-04-06 16:09:04 -0300

    Update bitPacking.cpp

--- bitPacking.cpp
+++ bitPacking.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <memory>
+#include <bitset>
 
 using namespace std;
 
@@ -21,30 +22,49 @@ void clearBit(uint64_t& src, int32_t pos) {
     src = src & ~(1LL << pos);
 }
 
-uint64_t bswap(uint64_t src, int8_t nBytes) {
-    array<nBytes, uint8_t> bytes;
+void swap(uint8_t& a, uint8_t& b) {
+    uint8_t aux = a;
+    a = b;
+    b = aux;
+}
 
-    for(int i = 0; i < nBytes/2; i++) {
+uint16_t bswap(uint16_t src) {
+    uint8_t* bytes = (uint8_t*)&src;
+    int32_t n      = sizeof(uint16_t);
 
+    for(int i = 0; i < n/2; i++) {
+        swap(bytes[i], bytes[n-1-i]);
     }
 
-    return src;
+    return *(uint16_t*)bytes;
+}
+
+uint32_t bswap(uint32_t src) {
+    uint8_t* bytes = (uint8_t*)&src;
+    int32_t n      = sizeof(uint32_t);
+
+    for(int i = 0; i < n/2; i++) {
+        swap(bytes[i], bytes[n-1-i]);
+    }
+
+    return *(uint32_t*)bytes;
+}
+
+uint64_t bswap(uint64_t src) {
+    uint8_t* bytes = (uint8_t*)&src;
+    int32_t n      = sizeof(uint64_t);
+
+    for(int i = 0; i < n/2; i++) {
+        swap(bytes[i], bytes[n-1-i]);
+    }
+
+    return *(uint64_t*)bytes;
 } 
-
-// a b c d e
-// 
-
-// 0101 shift <-> 1000 | 0101 = 11101
-// 1100 shift <-> 0011 | 0000 <-> 0011
-//                                1100
-
-// 1001 1100
-// 1100 1011
 
 class Buffer {
 public:
-    Buffer() {
-
+    Buffer(int32_t size) : size(size), index(0) {
+        data = new uint8_t[this->size];
     }
 
     void writeUint8(uint8_t arg) {
@@ -80,16 +100,23 @@ public:
     }
 
 private:
+    bool endianess() {
+        this->isMachineLittleEndian = 1&1 == 1 ? 1 : 0;
+    }
+
     uint8_t*  data;
     int32_t   size;
     uint32_t  index;
-    bool      endianess;
+    bool      isMachineLittleEndian;
 };
 
 int main() {
-    uint16_t a = 0b1111111100000000;
-    // a = bswap(a, 2);
-    // printBits(a, 16);
-    bswap(a, 2);
+    uint16_t x = 1;
+    // x = bswap(x);
+    printBits(x, 16);
+    cout << endl;
+    int p = (*(uint8_t*)&x);
+    cout << p << endl;
+    // cout << (bswap(uint32_t(1))&1) << endl;
     return 0;
 }
